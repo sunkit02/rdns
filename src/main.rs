badb7895140665c8b8cc65be76c7105362c1191b
Sun Kit Tsui <sunkittsui@gmail.com>
2024-06-15 03:40:48 -0400

    feat: Implement retrying truncated queries with TCP

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{env, net::UdpSocket};
+use std::{
+    env,
+    io::{Read, Write},
+    net::{TcpStream, UdpSocket},
+};
 
 use rdns::{view::View, DecodeBinary, DnsPacket, DnsQtype, DnsQuery, EncodeBinary};
 
@@ -15,12 +19,41 @@ fn main() {
 
     let socket = UdpSocket::bind("0.0.0.0:6679").unwrap();
 
-    let response = lookup_domain(domain_name, A_ROOT_SERVER, &socket);
+    let response = match lookup_domain(domain_name, A_ROOT_SERVER, &socket) {
+        Ok(response) => response,
+        Err(_) => {
+            #[cfg(debug_assertions)]
+            eprintln!("->> DEBUG: Response truncated, using TCP.");
+            let mut stream = TcpStream::connect(A_ROOT_SERVER).unwrap();
+            lookup_domain_tcp(domain_name, &mut stream)
+        }
+    };
 
     pretty_print_response(&response);
 }
 
-fn lookup_domain(domain_name: &str, dns_server: &str, socket: &UdpSocket) -> DnsPacket {
+fn lookup_domain_tcp(domain_name: &str, stream: &mut TcpStream) -> DnsPacket {
+    let query = DnsQuery::new(domain_name, DnsQtype::A).encode();
+    let query_len = (query.len() as u16).to_be_bytes();
+    let mut bytes = query_len.to_vec();
+    bytes.extend(query);
+
+    stream.write_all(&bytes).unwrap();
+
+    let mut buffer = [0u8; 1024];
+    let received = stream.read(&mut buffer).unwrap();
+
+    // Ignore first two length bytes
+    let mut view = View::new(&buffer[2..received]);
+
+    let packet = DnsPacket::decode(&mut view);
+
+    assert!(view.is_at_end());
+
+    packet
+}
+
+fn lookup_domain(domain_name: &str, dns_server: &str, socket: &UdpSocket) -> Result<DnsPacket, ()> {
     let query = DnsQuery::new(domain_name, DnsQtype::A).encode();
 
     let sent = socket.send_to(query.as_slice(), dns_server).unwrap();
@@ -36,14 +69,37 @@ fn lookup_domain(domain_name: &str, dns_server: &str, socket: &UdpSocket) -> Dns
 
     let mut view = View::new(&buffer[..received]);
 
-    DnsPacket::decode(&mut view)
+    let packet = DnsPacket::decode(&mut view);
+
+    assert!(view.is_at_end());
+
+    if packet.header.flags.is_truncated() {
+        Err(())
+    } else {
+        Ok(packet)
+    }
 }
 
 fn pretty_print_response(packet: &DnsPacket) {
-    let opcode = "unimplemented";
-    let status = "unimplemented";
+    let opcode = packet.header.flags.opcode().to_string();
+    let status = packet
+        .header
+        .flags
+        .response_code()
+        .to_string()
+        .to_uppercase();
     let id = packet.header.id;
-    let flags = "unimplemented";
+    let flags =
+        packet
+            .header
+            .flags
+            .get_set_flags_abbrv()
+            .iter()
+            .fold(String::new(), |mut acc, &f| {
+                acc.push_str(f);
+                acc.push(' ');
+                acc
+            });
     let queries = packet.header.num_questions;
     let answers = packet.header.num_answers;
     let authorities = packet.header.num_authorities;
